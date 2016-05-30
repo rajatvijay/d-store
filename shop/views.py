940969aff4548f960585da71d14e0ec2a26d74dc
rajatvijay <rajatvijay5@gmail.com>
2016-05-30 05:57:42 -0700

    Added the product detail view

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 
+
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
@@ -11,3 +12,9 @@ def product_list(request, category_slug=None):
     return render(request, 'shop/product/list.html', {'category': category,
                                                       'categories': categories,
                                                       'products': products})
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    return render(request,
+                  'shop/product/detail.html',
+                  {'product': product})
