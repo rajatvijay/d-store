a81b1f660994bba7ccc309537603159bbb54c7b6
rajatvijay <rajatvijay5@gmail.com>
2016-05-30 05:52:54 -0700

    Added get_absolute_url for the category and the product model

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.urlresolvers import reverse
+
 
 
 class Category(models.Model):
@@ -12,6 +14,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
 
 
 class Product(models.Model):
@@ -32,3 +37,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
