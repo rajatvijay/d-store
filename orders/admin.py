76fa8cf025a26842452be85ababcb37c49598761
rajatvijay <rajatvijay5@gmail.com>
2016-05-31 10:51:09 -0700

    Created a generic model action to export model info and data as csv

--- orders/admin.py
+++ orders/admin.py
@@ -1,10 +1,36 @@
+import csv
+import datetime
 from django.contrib import admin
+from django.http import HttpResponse
 from .models import Order, OrderItem
 
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
     raw_id_fields = ['product']
+
+
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename={}.csv'.format(opts.verbose_name)
+    writer = csv.writer(response)
+
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    # write the header row
+    writer.writerow([field.verbose_name for field in fields])
+    # write data rows
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+export_to_csv.short_description = 'Export to CSV'
+
 
 
 class OrderAdmin(admin.ModelAdmin):
