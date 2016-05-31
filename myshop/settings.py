00f5b6fd7c2768b902dfb4ecc4c69c3cb990387d
rajatvijay <rajatvijay5@gmail.com>
2016-05-31 02:57:24 -0700

    Added orders app and registered it in the settings

--- myshop/settings.py
+++ myshop/settings.py
@@ -66,6 +66,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'cart.context_processors.cart',
+                'orders',
             ],
         },
     },
