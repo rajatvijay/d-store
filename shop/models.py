b9e2327ebe97d842aac903c1b6591b1d68b46615
rajatvijay <rajatvijay5@gmail.com>
2016-06-01 03:35:35 -0700

    Removed unwanted fields  from the shop model

--- shop/models.py
+++ shop/models.py
@@ -5,9 +5,6 @@ from parler.models import TranslatableModel, TranslatedFields
 
 
 class Category(TranslatableModel):
-    name = models.CharField(max_length=200, db_index=True)
-    slug = models.SlugField(max_length=200, db_index=True, unique=True)
-
     translations = TranslatedFields (
         name=models.CharField(max_length=200, db_index=True),
         slug=models.SlugField(max_length=200, db_index=True, unique=True),
@@ -27,10 +24,7 @@ class Category(TranslatableModel):
 
 class Product(TranslatableModel):
     category = models.ForeignKey(Category, related_name='products')
-    name = models.CharField(max_length=200, db_index=True)
-    slug = models.SlugField(max_length=200, db_index=True)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
-    description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
     available = models.BooleanField(default=True)
