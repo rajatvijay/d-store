0189b7798a8360b13977513f9bc4d40836d0e339
rajatvijay <rajatvijay5@gmail.com>
2016-06-01 02:54:55 -0700

    Added translations for urls

--- myshop/urls.py
+++ myshop/urls.py
@@ -18,14 +18,15 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
+from django.utils.translation import gettext_lazy as _
 
 urlpatterns = i18n_patterns(
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^cart/', include('cart.urls', namespace='cart')),
-    url(r'^orders/', include('orders.urls', namespace='orders')),
-    url(r'^paypal/', include('paypal.standard.ipn.urls')),
-    url(r'^payment/', include('payment.urls', namespace='payment')),
-    url(r'^coupons/', include('coupons.urls', namespace='coupons')),
+    url(_(r'^cart/'), include('cart.urls', namespace='cart')),
+    url(_(r'^orders/'), include('orders.urls', namespace='orders')),
+    url(_(r'^paypal/'), include('paypal.standard.ipn.urls')),
+    url(_(r'^payment/'), include('payment.urls', namespace='payment')),
+    url(_(r'^coupons/'), include('coupons.urls', namespace='coupons')),
     url(r'^rosetta/', include('rosetta.urls')),
     url(r'^', include('shop.urls', namespace='shop')),
 )
